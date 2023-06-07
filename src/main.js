d85616a014a6cafce7c8286c718d81dfbfba4a41
itkulichkov <KulDmiVi@gmail.com>
2023-06-07 13:14:24 +0300

    Добавлена директива click -outside

--- src/main.js
+++ src/main.js
@@ -9,12 +9,26 @@ import "bootstrap/dist/js/bootstrap.js"
 import "bootstrap-select/dist/js/bootstrap-select.js"
 
 import setupInterceptors from './services/setupInterceptors';
+
 setupInterceptors(store);
 
-createApp(App)
+const app = createApp(App)
+
+app
     .use(router)
     .use(store)
     .mount('#app')
 
-
-
+app.directive('click-outside', {
+    mounted(element, { value }){
+        element.ClickOutside = function (event){
+            if (!(element === event.target || element.contains(event.target))){
+                value(event);
+            }
+        }
+        document.body.addEventListener('click', element.ClickOutside)
+    },
+    unmounted(element){
+        document.body.removeEventListener('click', element.ClickOutside)
+    }
+})
